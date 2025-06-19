acc83188b4bec2af9a723df965edec87bc06ad97
Oget Charlene <charlene.oget@epitech.eu>
2025-06-19 14:45:05 +0200

    adding static color palette

---
+++ my-customizer/src/components/ColorCustomize.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex h-16 ml-2">
+        <button class="bg-black w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-red-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-orange-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-yellow-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-green-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-emerald-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-sky-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-violet-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-fuchsia-500 w-16 border border-gray-400 rounded shadow-md"></button>
+        <button class="bg-gray-500 w-16 border border-gray-400 rounded shadow-md"></button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- my-customizer/src/components/Footer.vue
+++ my-customizer/src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <footer class="fixed bottom-0 flex items-center justify-center w-screen h-20 bg-gradient-to-r from-white via-purple-200 to-pink-200 text-gray-700 shadow-md">
-    
+    <footer class="fixed bottom-0 flex items-center w-screen h-20 bg-gradient-to-r from-white via-purple-200 to-pink-200 text-gray-700 shadow-md">
+        <ColorCustomize />
     </footer>
 </template>
 
 <script setup>
-
+    import ColorCustomize from './ColorCustomize.vue';
 </script>
