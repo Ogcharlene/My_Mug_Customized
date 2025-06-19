6dbdb01883bf8a6979b19c861c98935602ee67b3
Oget Charlene <charlene.oget@epitech.eu>
2025-06-19 13:27:00 +0200

    adding a footer component

--- my-customizer/src/App.vue
+++ my-customizer/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import CanvasModel from './components/CanvasModel.vue';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header />
   <CanvasModel />
+  <Footer />
  
 </template>
---
+++ my-customizer/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="fixed bottom-0 flex items-center justify-center w-screen h-20 bg-gradient-to-r from-white via-purple-200 to-pink-200 text-gray-700 shadow-md">
+    
+    </footer>
+</template>
+
+<script setup>
+
+</script>
